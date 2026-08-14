7bdcd53ef43fe1904c0734a1663acd24b6d76ea1
Marcel Assis <marcelcfa@outlook.com>
2026-08-14 16:16:42 -0300

    feat: add Period with covers?, days and overlaps?

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,5 @@
 class Period
+  attr_reader :start_at, :end_at
 
   def initialize(start_at, end_at)
     @start_at = start_at
@@ -8,7 +9,15 @@ class Period
   def covers?(date)
     return date >= @start_at if @end_at.nil?
     date >= @start_at && date <= @end_at
+  end
 
+  def days
+    (@end_at - @start_at) + 1
+  end
+
+  def overlaps?(other)
+    return true if end_at.nil?
+    end_at >= other.start_at && start_at <= other.end_at
   end
 
 
